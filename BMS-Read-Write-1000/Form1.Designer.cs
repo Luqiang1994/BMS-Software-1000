deba2c4f09dc58ea993e7674136fdeed4ea4cd63
Luqiang1994 <925722021@qq.com>
2026-06-09 16:10:06 +0800

    first commit

---
+++ BMS-Read-Write-1000/Form1.Designer.cs
@@ -0,0 +1,408 @@
+﻿namespace BMS_Read_Write_1000
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            tabControl1 = new TabControl();
+            tabPage1 = new TabPage();
+            tabPage2 = new TabPage();
+            groupBox1 = new GroupBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            comboBox1 = new ComboBox();
+            comboBox2 = new ComboBox();
+            comboBox3 = new ComboBox();
+            groupBox2 = new GroupBox();
+            comboBox4 = new ComboBox();
+            comboBox5 = new ComboBox();
+            comboBox6 = new ComboBox();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            groupBox3 = new GroupBox();
+            comboBox7 = new ComboBox();
+            comboBox8 = new ComboBox();
+            comboBox9 = new ComboBox();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            comboBox10 = new ComboBox();
+            label11 = new Label();
+            label12 = new Label();
+            comboBox11 = new ComboBox();
+            comboBox12 = new ComboBox();
+            tabControl1.SuspendLayout();
+            tabPage1.SuspendLayout();
+            groupBox1.SuspendLayout();
+            groupBox2.SuspendLayout();
+            groupBox3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            tabControl1.Controls.Add(tabPage1);
+            tabControl1.Controls.Add(tabPage2);
+            tabControl1.Location = new Point(3, 2);
+            tabControl1.Name = "tabControl1";
+            tabControl1.SelectedIndex = 0;
+            tabControl1.Size = new Size(749, 511);
+            tabControl1.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            tabPage1.Controls.Add(groupBox3);
+            tabPage1.Controls.Add(groupBox2);
+            tabPage1.Controls.Add(groupBox1);
+            tabPage1.Location = new Point(4, 26);
+            tabPage1.Name = "tabPage1";
+            tabPage1.Padding = new Padding(3);
+            tabPage1.Size = new Size(741, 481);
+            tabPage1.TabIndex = 0;
+            tabPage1.Text = "实时监控";
+            tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            tabPage2.Location = new Point(4, 26);
+            tabPage2.Name = "tabPage2";
+            tabPage2.Padding = new Padding(3);
+            tabPage2.Size = new Size(939, 554);
+            tabPage2.TabIndex = 1;
+            tabPage2.Text = "参数配置";
+            tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(comboBox3);
+            groupBox1.Controls.Add(comboBox2);
+            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(label3);
+            groupBox1.Controls.Add(label1);
+            groupBox1.Location = new Point(8, 6);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(229, 119);
+            groupBox1.TabIndex = 0;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "过充保护";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(30, 19);
+            label1.Name = "label1";
+            label1.Size = new Size(86, 17);
+            label1.TabIndex = 0;
+            label1.Text = "过充电压(mV):";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(6, 53);
+            label2.Name = "label2";
+            label2.Size = new Size(110, 17);
+            label2.TabIndex = 0;
+            label2.Text = "过充恢复电压(mV):";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(43, 87);
+            label3.Name = "label3";
+            label3.Size = new Size(73, 17);
+            label3.TabIndex = 0;
+            label3.Text = "过充延时(s):";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(132, 16);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(78, 25);
+            comboBox1.TabIndex = 1;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(132, 50);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(78, 25);
+            comboBox2.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            comboBox3.FormattingEnabled = true;
+            comboBox3.Location = new Point(132, 84);
+            comboBox3.Name = "comboBox3";
+            comboBox3.Size = new Size(78, 25);
+            comboBox3.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            groupBox2.Controls.Add(comboBox4);
+            groupBox2.Controls.Add(comboBox5);
+            groupBox2.Controls.Add(comboBox6);
+            groupBox2.Controls.Add(label4);
+            groupBox2.Controls.Add(label5);
+            groupBox2.Controls.Add(label6);
+            groupBox2.Location = new Point(3, 131);
+            groupBox2.Name = "groupBox2";
+            groupBox2.Size = new Size(229, 119);
+            groupBox2.TabIndex = 0;
+            groupBox2.TabStop = false;
+            groupBox2.Text = "过放保护";
+            // 
+            // comboBox4
+            // 
+            comboBox4.FormattingEnabled = true;
+            comboBox4.Location = new Point(132, 84);
+            comboBox4.Name = "comboBox4";
+            comboBox4.Size = new Size(78, 25);
+            comboBox4.TabIndex = 1;
+            // 
+            // comboBox5
+            // 
+            comboBox5.FormattingEnabled = true;
+            comboBox5.Location = new Point(132, 50);
+            comboBox5.Name = "comboBox5";
+            comboBox5.Size = new Size(78, 25);
+            comboBox5.TabIndex = 1;
+            // 
+            // comboBox6
+            // 
+            comboBox6.FormattingEnabled = true;
+            comboBox6.Location = new Point(132, 16);
+            comboBox6.Name = "comboBox6";
+            comboBox6.Size = new Size(78, 25);
+            comboBox6.TabIndex = 1;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(6, 53);
+            label4.Name = "label4";
+            label4.Size = new Size(110, 17);
+            label4.TabIndex = 0;
+            label4.Text = "过放恢复电压(mV):";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(43, 87);
+            label5.Name = "label5";
+            label5.Size = new Size(73, 17);
+            label5.TabIndex = 0;
+            label5.Text = "过放延时(s):";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(30, 19);
+            label6.Name = "label6";
+            label6.Size = new Size(86, 17);
+            label6.TabIndex = 0;
+            label6.Text = "过放电压(mV):";
+            // 
+            // groupBox3
+            // 
+            groupBox3.Controls.Add(comboBox12);
+            groupBox3.Controls.Add(comboBox7);
+            groupBox3.Controls.Add(comboBox11);
+            groupBox3.Controls.Add(comboBox10);
+            groupBox3.Controls.Add(comboBox8);
+            groupBox3.Controls.Add(comboBox9);
+            groupBox3.Controls.Add(label12);
+            groupBox3.Controls.Add(label10);
+            groupBox3.Controls.Add(label7);
+            groupBox3.Controls.Add(label11);
+            groupBox3.Controls.Add(label8);
+            groupBox3.Controls.Add(label9);
+            groupBox3.Location = new Point(3, 256);
+            groupBox3.Name = "groupBox3";
+            groupBox3.Size = new Size(229, 221);
+            groupBox3.TabIndex = 0;
+            groupBox3.TabStop = false;
+            groupBox3.Text = "过流保护";
+            // 
+            // comboBox7
+            // 
+            comboBox7.FormattingEnabled = true;
+            comboBox7.Location = new Point(132, 84);
+            comboBox7.Name = "comboBox7";
+            comboBox7.Size = new Size(78, 25);
+            comboBox7.TabIndex = 1;
+            // 
+            // comboBox8
+            // 
+            comboBox8.FormattingEnabled = true;
+            comboBox8.Location = new Point(132, 50);
+            comboBox8.Name = "comboBox8";
+            comboBox8.Size = new Size(78, 25);
+            comboBox8.TabIndex = 1;
+            // 
+            // comboBox9
+            // 
+            comboBox9.FormattingEnabled = true;
+            comboBox9.Location = new Point(132, 16);
+            comboBox9.Name = "comboBox9";
+            comboBox9.Size = new Size(78, 25);
+            comboBox9.TabIndex = 1;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(19, 53);
+            label7.Name = "label7";
+            label7.Size = new Size(97, 17);
+            label7.TabIndex = 0;
+            label7.Text = "充电过流延时(s):";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(28, 87);
+            label8.Name = "label8";
+            label8.Size = new Size(93, 17);
+            label8.TabIndex = 0;
+            label8.Text = "放电过流1(mA):";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(30, 19);
+            label9.Name = "label9";
+            label9.Size = new Size(86, 17);
+            label9.TabIndex = 0;
+            label9.Text = "充电过流(mA):";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(12, 121);
+            label10.Name = "label10";
+            label10.Size = new Size(104, 17);
+            label10.TabIndex = 0;
+            label10.Text = "放电过流1延时(s):";
+            // 
+            // comboBox10
+            // 
+            comboBox10.FormattingEnabled = true;
+            comboBox10.Location = new Point(132, 118);
+            comboBox10.Name = "comboBox10";
+            comboBox10.Size = new Size(78, 25);
+            comboBox10.TabIndex = 1;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(28, 151);
+            label11.Name = "label11";
+            label11.Size = new Size(93, 17);
+            label11.TabIndex = 0;
+            label11.Text = "放电过流2(mA):";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(12, 185);
+            label12.Name = "label12";
+            label12.Size = new Size(104, 17);
+            label12.TabIndex = 0;
+            label12.Text = "放电过流2延时(s):";
+            // 
+            // comboBox11
+            // 
+            comboBox11.FormattingEnabled = true;
+            comboBox11.Location = new Point(132, 182);
+            comboBox11.Name = "comboBox11";
+            comboBox11.Size = new Size(78, 25);
+            comboBox11.TabIndex = 1;
+            // 
+            // comboBox12
+            // 
+            comboBox12.FormattingEnabled = true;
+            comboBox12.Location = new Point(132, 148);
+            comboBox12.Name = "comboBox12";
+            comboBox12.Size = new Size(78, 25);
+            comboBox12.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(947, 584);
+            Controls.Add(tabControl1);
+            Name = "Form1";
+            Text = "Form1";
+            tabControl1.ResumeLayout(false);
+            tabPage1.ResumeLayout(false);
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            groupBox2.ResumeLayout(false);
+            groupBox2.PerformLayout();
+            groupBox3.ResumeLayout(false);
+            groupBox3.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private TabControl tabControl1;
+        private TabPage tabPage1;
+        private GroupBox groupBox1;
+        private Label label2;
+        private Label label3;
+        private Label label1;
+        private TabPage tabPage2;
+        private GroupBox groupBox3;
+        private ComboBox comboBox7;
+        private ComboBox comboBox8;
+        private ComboBox comboBox9;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private GroupBox groupBox2;
+        private ComboBox comboBox4;
+        private ComboBox comboBox5;
+        private ComboBox comboBox6;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private ComboBox comboBox3;
+        private ComboBox comboBox2;
+        private ComboBox comboBox1;
+        private ComboBox comboBox12;
+        private ComboBox comboBox11;
+        private ComboBox comboBox10;
+        private Label label12;
+        private Label label10;
+        private Label label11;
+    }
+}
---
+++ BMS-Read-Write-1000/Form1.cs
@@ -0,0 +1,10 @@
+namespace BMS_Read_Write_1000
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
